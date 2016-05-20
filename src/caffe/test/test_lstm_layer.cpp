b182593b5070d279250127b155a808d618732b25
Shean <yshean@gmail.com>
2016-05-20 18:22:28 +0800

    fix test_lstm_layer.cpp header

--- src/caffe/test/test_lstm_layer.cpp
+++ src/caffe/test/test_lstm_layer.cpp
@@ -7,6 +7,7 @@
 #include "caffe/common.hpp"
 #include "caffe/filler.hpp"
 #include "caffe/layers/lstm_layer.hpp"
+#include "caffe/layers/lstm_unit_layer.hpp"
 
 #include "caffe/test/test_caffe_main.hpp"
 #include "caffe/test/test_gradient_check_util.hpp"
